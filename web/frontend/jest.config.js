3f0def5098a6441c2ffdd36a44fbda389ed52509
Ben Coleman <benc.uk@gmail.com>
2023-12-20 23:14:50 +0000

    Fix tests and CI

--- web/frontend/jest.config.js
+++ web/frontend/jest.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputName: 'unit-tests-frontend.xml',
-        outputDirectory: '../../output'
-      }
-    ]
-  ]
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
 }
